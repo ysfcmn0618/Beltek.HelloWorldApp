f0b92dc346598ec65ff0aa7232ab56f299cfae5a
yusuf çimen <yusufcimen90@windowslive.com>
2024-04-20 13:02:44 +0300

    değişken tanımlayıp jullanıcan isim bilgisi istenip hoşgeldin karşılaması yapıldı.

--- Beltek.HelloWorldApp/Program.cs
+++ Beltek.HelloWorldApp/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("Lütfen Adınızı Giriniz :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hoşgeldin, "+name);
+            Console.ReadKey();
         }
     }
 }
