dbf6bbbb0604bd8b79aa65c95290ac4ea098526d
yusuf çimen <yusufcimen90@windowslive.com>
2024-04-21 10:26:14 +0300

    değişken tanımlama

--- Beltek.HelloWorldApp/Program.cs
+++ Beltek.HelloWorldApp/Program.cs
@@ -2,13 +2,21 @@
 {
     internal class Program
     {
+        static int stat = 10;
+
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
-            Console.WriteLine("Lütfen Adınızı Giriniz :");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hoşgeldin, "+name);
-            Console.ReadKey();
+            ////Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Lütfen Adınızı Giriniz :");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hoşgeldin, "+name);
+            //Console.ReadKey();
+
+            const byte abc = 30;
+            byte a = 1, c;
+            double b = 2, d;
+            c = 3;
+            d = 4;
         }
     }
 }
