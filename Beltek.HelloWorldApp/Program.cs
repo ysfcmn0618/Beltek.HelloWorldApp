12ef83cf72c6cbfb454e99b24877eb02d9df09c2
yusuf çimen <yusufcimen90@windowslive.com>
2024-04-21 11:23:07 +0300

    string ekrana değer yazdırma yöntemleri

--- Beltek.HelloWorldApp/Program.cs
+++ Beltek.HelloWorldApp/Program.cs
@@ -15,8 +15,17 @@
             const byte abc = 30;
             byte a = 1, c;
             double b = 2, d;
-            c = 3;
+            //Int16 value = 3;
+            c = 3;//Explicit = açık açık apılan tanımlamalar / Clearly definiations name 
             d = 4;
+
+            var sayi = 5; //implicit örtülü tanımlama tipi içeriğin tipini sysyem belirler /closed definitions this value type verdict is given to the sysyem
+
+
+            string name = "Ahmet", surname = "meğmet";
+            Console.WriteLine("Welcome " + " " + name + " " + surname);
+            Console.WriteLine("Welcome {0} {1}",name,surname);
+            Console.WriteLine($"Welcome {name} {surname}");
         }
     }
 }
