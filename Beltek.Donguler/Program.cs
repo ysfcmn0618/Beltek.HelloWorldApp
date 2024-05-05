4e175484eb9debf95f66a2e4f6ca6a3e993a457c
yusuf çimen <yusufcimen90@windowslive.com>
2024-05-05 12:56:14 +0300

    5.5.24 döngü

---
+++ Beltek.Donguler/Program.cs
@@ -0,0 +1,136 @@
+﻿namespace Beltek.Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Başlangıç değerini giriniz = ");
+            //byte bas = byte.Parse(Console.ReadLine());
+            //Console.WriteLine("Bitiş değerini giriniz = ");
+            //byte bitis = byte.Parse(Console.ReadLine());
+            //Console.WriteLine("Tek - Çift ?");
+            //string secim = Console.ReadLine().ToLower();
+
+            //int sonuc = 0;
+            //if (bas > bitis)
+            //{
+            //    byte temp = bas;
+            //    bas = bitis;
+            //    bitis = temp;
+            //}
+
+
+            //for (int i = bas; i < bitis; i++)
+            //{
+            //    if (secim == "çift" && i % 2 == 0  )    //Başlangıç ve bitiş arasındaki çift sayıları yazar
+            //    {
+
+            //        Console.WriteLine($"Çift {i}");
+            //        i+=2;
+            //        sonuc += i;
+            //    }
+            //    else if ( secim == "tek" &&  i % 2 == 1 )
+            //    {
+            //        sonuc += i;
+            //        Console.WriteLine($"Tek {i}");
+            //    }
+            //}
+
+            //if (i % 2 == (secim == "tek" ? 1 : 0))   // i nin yai başlangıç değerimizin mod 2 sinden kalan tek mi diye bakar ve "TEK" ise mod 2 sini 1 mi diye kontrl eder  ve şartı yerine getirir .
+            //{
+            //    sonuc += i;
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine($"İşlemin sonucu :{sonuc}");
+
+            ////üslü sayıların sonucu na göre işlem yapma ****üslü sayıları tam sayıya çevirme
+            //Console.WriteLine("Taban değeri giriniz = ");
+            //byte taban = byte.Parse(Console.ReadLine());
+            //Console.WriteLine("Üst değerini giriniz = ");
+            //byte us = byte.Parse(Console.ReadLine());
+            //int sonuc = 1;
+            //for (int i = 0; i < us; i++)
+            //{
+            //    sonuc *= taban;
+            //}
+            //Console.WriteLine(sonuc);
+
+
+            ////Faktoriyel hesabı yapma
+            //Console.WriteLine("Sayı giriniz = ");
+            //int sayi = int.Parse(Console.ReadLine());
+            //int sonuc = 1;
+
+            //for (int i = 1; i <= sayi; i++)
+            //{
+            //    sonuc *= i;
+            //    //if (i== sayi)
+            //    //{
+            //    //    Console.WriteLine($"Sonuç = {sonuc}");
+            //    //}
+            //}
+            //Console.WriteLine($"Sonuç = {sonuc}");
+
+
+            ////ASAL sayı hesabı
+            //int kontrol = 0;
+            //Console.Write("Sayı Girin : ");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 2; i <= sayi / 2; i++)
+            //{
+            //    if (sayi % i == 0)
+            //    {
+            //        kontrol++;
+            //        break;
+            //    }
+            //}
+
+            //Console.WriteLine(kontrol == 0 ? "Girdiğiniz sayı asaldır." : "Girdiğiniz sayı asal değildir.");
+            //Console.ReadKey();
+
+            //string user;
+            //for (; ; )
+            //{
+            //    Console.Write("Kullancı adı giriniz : ");
+            //    user = Console.ReadLine().ToLower();
+            //    Console.Write("Şifre  giriniz : ");
+            //    string password = Console.ReadLine().ToLower();
+            //    if (user == "admin" && password == "123")
+            //    {
+            //        Console.WriteLine("Hoşgeldin admin");
+            //        break;
+            //    }
+            //    else
+            //        Console.WriteLine("Hatalı giriş tekrar deneyin");
+
+            //}
+
+
+            int max = int.MinValue, ciftToplam = 0;
+            for (; ; )//Sonsuz döngü
+            {
+                Console.WriteLine("Sayı giriniz :");
+                int sayi = int.Parse(Console.ReadLine());
+                if (sayi > max)
+                {
+                    max = sayi;                    
+                }
+                if (sayi % 2 == 0)
+                {
+                    ciftToplam += sayi;
+                }
+                Console.WriteLine("Devam etmek istiyormusnuz? (e-h)");
+                char devam = char.Parse(Console.ReadLine().ToLower());
+                if (devam == 'h')
+                {
+                    Console.WriteLine($"Girilen en büyük sayı {max} \nGirilen Çift sayıların Toplamı : {ciftToplam}");
+                    break;//Gerekli şartlar sağlanınca sonsuz döngünün kırıldığı nokta
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
--- Beltek.HelloWorldApp/Program.cs
+++ Beltek.HelloWorldApp/Program.cs
@@ -2,11 +2,7 @@
 
 namespace Beltek.HelloWorldApp
 {
-    //ctrl + k d kodları düenler
-    //ctrl +k c comment satırı ekleme
-    //ctrl + k u uncomment 
-    //ctrl + . namespace using eklemek için açılır menü 
-    //ctrl + k s region gibi kod bloklarını oluşturabileceğimiz menü açılır.
+
     internal class Program
     {
 
@@ -89,39 +85,179 @@ namespace Beltek.HelloWorldApp
             #endregion
 
             #region 2. Hafta
+            //EXCEPTİON HANDLİNG
             // try catch blokların da hata önetimi uglanır oluşan her hata için bit catch bloğu oluşturulur ve yakalanan hata türünü göre hata yönetimi uygulanır.uygulanması gerekir.
-            try
-            {
-                Console.Write("Adınızı Giriniz :");
-                string name = Console.ReadLine();
-                Console.Write("Soyadınızı Giriniz :");
-                string surname = Console.ReadLine();
-                Console.Write("Yaşınızı Giriniz :");
-                byte age = byte.Parse(Console.ReadLine());
 
-                Console.WriteLine($"Hoşgeldin Canım {name} {surname} neden geciktin?!!\n {age} yıl beklemeye ne gerek vardı.!!!");
+            //try
+            //{
+            //    Console.Write("Adınızı Giriniz :");
+            //    string name = Console.ReadLine();
+            //    Console.Write("Soyadınızı Giriniz :");
+            //    string surname = Console.ReadLine();
+            //    Console.Write("Yaşınızı Giriniz :");
+            //    byte age = byte.Parse(Console.ReadLine());
 
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("0-255 arası bir değer giriniz!!!");
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Yaşınızı sayı olarak giriniz lütfen!!!");
-            }
-            catch (Exception)
-            {
+            //    Console.WriteLine($"Hoşgeldin Canım {name} {surname} neden geciktin?!!\n {age} yıl beklemeye ne gerek vardı.!!!");
 
-                Console.WriteLine("Bilinmeyem Bir Hata Oluştu!!!!");
+            //}
+            //catch (OverflowException)
+            //{
+            //    Console.WriteLine("0-255 arası bir değer giriniz!!!");
+            //}
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("Yaşınızı sayı olarak giriniz lütfen!!!");
+            //}
+            //catch (Exception)
+            //{
+
+            //    Console.WriteLine("Bilinmeyem Bir Hata Oluştu!!!!");
+            //}
+
+            //*******OPERATORLER (?: kARAR YAPILARI 2 olasılığı karşılaştırırken kullanılır)
+
+            //Console.WriteLine("Bir sayı giriniz :");
+            //int sayi1= int.Parse(Console.ReadLine());
+            //Console.WriteLine("Bir sayı daha giriniz :");
+            //int sayi2= int.Parse(Console.ReadLine());
+            //string sonuç = sayi1>sayi2 ? "İlk girilen sayı büyüktür" : "sayı 2 büyüktür yada eşittir";
+            //Console.WriteLine(sonuç);
+
+
+            //Console.WriteLine("Bir sayı giriniz :");
+            //int sayi1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Bir sayı daha giriniz :");
+            //int sayi2 = int.Parse(Console.ReadLine());
+
+            //if (sayi1 > sayi2)
+            //{
+            //    Console.WriteLine("Sayı 1 büyüktür");
+            //}
+            //else if (sayi1 < sayi2)
+            //{
+            //    Console.WriteLine("Sayı 2 büyüktür.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayılar eşittir");
+            //}
+
+
+            //hava nasıl ? Güzel : dışarı çık ? kötü evde otur ? sadece güzel ve kötü cevaplarını verebilirsiniz.
+
+            //Console.WriteLine("Bu gün hava nasıl?");
+            //string weather = Console.ReadLine().ToLower();
+            //if (weather == "güzel")
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine("Kaç derece hava sıcaklığı : ");
+            //        sbyte wearm = sbyte.Parse(Console.ReadLine());
+
+            //        //Kaç derece <10 soğuk evde otur 10-30 Hava güzel dışarı çık >30 çok sıcak evde otur
+
+            //        if (wearm < 10)
+            //        {
+            //            Console.WriteLine("Hava soğuk evde otur!!");
+
+            //        }
+            //        else if (wearm > 10 && wearm < 30)
+            //        {
+            //            Console.WriteLine("Hava güzel dışarı çık");
+
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Hava çok sıcak evde otur");
+            //        }
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("Sıcaklığı rakam olarak giriniz");
+            //    }
+            //    catch (OverflowException)
+            //    {
+            //        Console.WriteLine("Bu sıcaklıkta nasıl hayatta kalabildin!!!!");
+            //    }
+            //    catch (Exception)
+            //    {
+
+            //        throw;
+            //    }
+            //}
+            //else if (weather == "kötü")
+            //{
+            //    Console.WriteLine("Evde otur!!!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sadece Güzel  ve  Kötü ifadelerini verebilirsiniz!!!");
+            //}
+
+            //******SWİTCH CASE YAPIILARI 
+
+
+            //Console.WriteLine("apmak istediğiniz işlemi seçiniz \n1- Havale işlemleri \n2- Eft işlemleri\n3-Internet bankacılığı\n4- Müşteri hizmetleri");
+            //byte chose = byte.Parse(Console.ReadLine());
+
+            //switch (chose)
+            //{
+            //    case 1: Console.WriteLine("Havale işlemleri"); break;
+            //    case 2: Console.WriteLine("Eft işlemleri "); break;
+            //    case 3: Console.WriteLine("İnternet bankacılığı"); break;
+            //    case 4: Console.WriteLine("Müşteri Hizmetleri"); break;
+            //    default:
+            //        Console.WriteLine("Yanlış seçim yaptınız");
+            //        break;
+            //}
+
+            ///***ALAN HESABI YAPTIRMA
+
+            Console.WriteLine("Alanının hesaplanmasını istediğiniz şekli seçiniz: \n1-Kare \n2- Daire \n3-Üçgen");
+            byte sekil = byte.Parse(Console.ReadLine());
+            int kkenar, rcap, taban, yukseklik;
+            switch (sekil)
+            {
+                case 1:
+                    Console.WriteLine("Karenin kenar uzunluğunu giriniz : ");
+                    kkenar = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Karenin alanı:{kkenar*kkenar}");
+                    break;
+                case 2:
+                    Console.WriteLine("Yarı çap giriniz : ");
+                    rcap = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Dairenin çapı :{Math.PI * rcap * rcap}");
+                    break;
+                case 3:
+                    Console.WriteLine("Taban ölçüsünü giriniz : ");
+                    taban = int.Parse(Console.ReadLine());
+                    Console.WriteLine("yükseklik ölçüsünü giriniz :");
+                    yukseklik = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Üçgenin alanı :{taban * yukseklik / 2}");
+                    break;
+                default:
+                    Console.WriteLine("Şu an başka bir şeklimiz mevcut değil!!!!");
+                    break;
             }
+
+
+
 
             #endregion
 
-            Console.WriteLine("Merhaba !!");
-            Console.ReadKey();
+            //Console.WriteLine("Merhaba !!");
+            //Console.ReadKey();
+
+
+
         }
     }
 }
 //proje içerisinde genel tanımlamaları otomatik kullanmayı kaldırmak için proje üzerinde sağ tık/özellikler /global using /implisint global using kaldırdığımız da geçerli proje içerisinde System gibi temel namespacelerin otomatik tanımlamasını kaldırır. ve manuel olarak eklenmesi gerekir.
-//intellisense 
+//intellisense
+//ctrl + k d kodları düenler
+//ctrl +k c comment satırı ekleme
+//ctrl + k u uncomment 
+//ctrl + . namespace using eklemek için açılır menü 
+//ctrl + k s region gibi kod bloklarını oluşturabileceğimiz menü açılır.
+//Debug modda breakpointtten programı adım adım ilrletmek için f10 kullanılır
